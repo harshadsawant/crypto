e0aad8302ef3cbc2988a768632fb4ddccc7364d7
harshadsawant <harshadsawant135@gmail.com>
2020-08-09 22:48:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from Crypto.PublicKey import RSA
 # creating a Flask app 
 app = Flask(__name__) 
   
-# on the terminal type: curl http://127.0.0.1:5000/ 
-# returns hello world when we use GET. 
-# returns the data that we send when we use POST. 
+# on the terminal type: curl http://127.0.0.1:5000/  
 @app.route('/', methods = ['GET', 'POST']) 
 def home(): 
     if(request.method == 'GET'): 
